0d0e1f4fb0c1140638e95d94720a9a8abc9b678e
Pablo <pablo.riutort@gmail.com>
2016-04-23 23:01:20 +0200

    Añade 3 tipos de index
    Explorar los index o páginas principales añadidas en este commit.
    La cuestión es elegir el que más nos guste y desarrollar a partir de
    ahí.
    Se accede añadiendo index2 o index3 a la url.

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index2/', views.index2, name='index2'),
+    url(r'^index3/', views.index3, name='index3'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
 from django.shortcuts import render
 
+# A continuación 3 posibles variaciones de index. Se accede añadiendo /index2 o /index3 a la url
 
 def index(request):
-    return HttpReponse("Index")
+    return render(request, "index.html")
+    
+
+def index2(request):
+    return render(request, "index2.html")
+    
+    
+def index3(request):
+    return render(request, "index3.html")
