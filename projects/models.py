febaa7c267a3c4e120e54fa1032260e26fcaf9f5
Pablo <pablo.riutort@gmail.com>
2016-05-06 16:20:49 +0200

    Modifica diagrama de la BD
    Añade nueva entidad: Evaluación. Esta consiste en 6 puntuaciones y 6
    comentarios sobre un proyecto.
    Quita la valoración de un principio.

--- projects/models.py
+++ projects/models.py
@@ -57,18 +57,34 @@ class Metrica(models.Model):
 
 class Principi(models.Model):
     nom = models.CharField(max_length=30)
-    valoracio = models.PositiveSmallIntegerField(validators=[MaxValueValidator(10)])
-    projecte = models.ManyToManyField(Projecte, through='Historial')
+    objectiu = models.ManyToManyField(Objectiu, related_name="principis_objectius")
     
     def __unicode__(self):
         return "%s" % (self.nom)
 
 
-class Historial(models.Model):
+class Evaluacio(models.Model):
     projecte = models.ForeignKey(Projecte, on_delete=models.CASCADE)
-    principi = models.ForeignKey(Principi, on_delete=models.CASCADE, null=True) # A cumplir un objetivo!
-    darrera_valoracio = models.PositiveSmallIntegerField(validators=[MaxValueValidator(10)])
+    
+    # puntuacions
+    puntuacio_responsabilitat = models.PositiveSmallIntegerField()
+    puntuacio_estrategia = models.PositiveSmallIntegerField()
+    puntuacio_adquisicio = models.PositiveSmallIntegerField()
+    puntuacio_rendiment = models.PositiveSmallIntegerField()
+    puntuacio_conformitat = models.PositiveSmallIntegerField()
+    puntuacio_conducta = models.PositiveSmallIntegerField()
+    
+    # comentaris
+    comentari_responsabilitat = models.TextField()
+    comentari_estrategia = models.TextField()
+    comentari_adquisicio = models.TextField()
+    comentari_rendiment = models.TextField()
+    comentari_conformitat = models.TextField()
+    comentari_conducta = models.TextField()
+    
+    # data de modificacio
+    data_modificacio = models.DateTimeField(auto_now=True)
     
     def __unicode__(self):
-        return "%s %s: %s" % (self.projecte, self.objectiu, self.darrera_valoracio)
+        return "Evaluació del projecte %s. %s" % (self.projecte, self.data_modificacio)
 
