b5bd52284e7ab0bbd603dc2de67e8ef7a7fbd531
David CG <dvd.cg92@gmail.com>
2016-05-22 14:09:45 +0200

    Acavat el formulari de creació de projectes.
    Falten les vistes per visualitzar, el formulari per editar i un boto per eliminar

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,22 @@
 from django import forms
 from .models import *
+from functools import partial
+
+ESTAT = (
+    ('PE','Pendent'),
+    ('PR','Progres'),
+    ('RE','Rebutjat'),
+    ('FI','Finalitzat'),
+)
+
+TIPUS = (
+    ('F2P','Free To Play'),
+    ('CO','Convencional'),
+    ('ALT','Altres'),
+)
+
+# El Datepicker s'activa al template mitjansant un script de jQuery
+DateInput = partial(forms.DateInput, {'class': 'datepicker'})
 
 class PrincipiForm(forms.Form):
     principi = forms.CharField(max_length=100)
@@ -7,10 +24,19 @@ class PrincipiForm(forms.Form):
 class ObjectiuForm(forms.Form):
 	objectiu = forms.CharField(max_length=100)
 	descripcio = forms.CharField(widget=forms.Textarea)
-	#principis = forms.ModelChoiceField(queryset=Principi.objects.all(), empty_label=None)
 
 class ProjecteForm(forms.Form):
-	asd = "asd"
+	projecte = forms.CharField(max_length=100)
+	descripcio = forms.CharField(widget=forms.Textarea)
+	presupost = forms.IntegerField()
+	estat = forms.ChoiceField(required=False, 
+		widget=forms.Select, choices=ESTAT)
+	tipus = forms.ChoiceField(required=False, 
+		widget=forms.Select, choices=TIPUS)
+	data_inici = forms.DateField(widget=DateInput())
+	data_fi = forms.DateField(widget=DateInput())
+	vMin = forms.IntegerField()
+	vMax = forms.IntegerField()
 
 class MetricaForm(forms.Form):
 	metrica = forms.CharField(max_length=100)
--- projects/views.py
+++ projects/views.py
@@ -5,17 +5,20 @@ from django.shortcuts import render
 from .models import *
 from .forms import *
 
-# A continuación 3 posibles variaciones de index. Se accede añadiendo /index2 o /index3 a la url
-
+# Retorna la view del index de la aplicació
 def index(request):
     return render(request, "index.html")
 
+
 # Principis
+
+# Retorna la view de la finestra de principis, on es veuen tots el resources creats d'aquest tipus.
 def principis(request):    
     return render(request, "Principis/principis.html")
 
+# Si el Request es GET, retorna la view de creació del resource amb el formulari buit.
+# Si el Request es POST, crea el request amb les dades obtingudes al formulari
 def crearPrincipi(request):
-	# Dades enviades desde el form
 	if request.method == 'POST':
 		form = PrincipiForm(request.POST)
 		if form.is_valid():
@@ -24,58 +27,79 @@ def crearPrincipi(request):
 			p = Principi(nom=nom)
 			p.save()
 			return render(request, 'index.html')
-	# Request de view de formulari
 	else:
 		form = PrincipiForm()
 	return render(request, 'Principis/crear.html', {'form': form})
 
+
 # Objectius
+
+# Retorna la view de la finestra d'objectius, on es veuen tots el resources creats d'aquest tipus.
 def objectius(request):
     return render(request, 'Objectius/objectius.html')
 
+# Si el Request es GET, retorna la view de creació del resource amb el formulari buit.
+# Si el Request es POST, crea el request amb les dades obtingudes al formulari
 def crearObjectiu(request):
-	# Dades enviades desde el form
 	if request.method == 'POST':
 		form = ObjectiuForm(request.POST)
 		if form.is_valid():
-			# Si el formulari és vàlid, cream el objectiu
+			# Si el formulari és vàlid, cream l'objectiu
 			nom = form.cleaned_data['objectiu']
 			descripcio = form.cleaned_data['descripcio']
 			o = Objectiu(nom=nom, descripcio=descripcio)
 			o.save()
 			return render(request, 'index.html')
-	# Request de view de formulari
 	else:
 		form = ObjectiuForm()
 	return render(request, 'Objectius/crear.html', {'form': form})
 
+
 # Projectes
+
+# Retorna la view de la finestra de projectes, on es veuen tots el resources creats d'aquest tipus.
 def projectes(request):
     return render(request, 'Projectes/projectes.html')
 
+# Si el Request es GET, retorna la view de creació del resource amb el formulari buit.
+# Si el Request es POST, crea el request amb les dades obtingudes al formulari
 def crearProjecte(request):
-	# Dades enviades desde el form
 	if request.method == 'POST':
-		form = PrincipiForm(request.POST)
+		form = ProjecteForm(request.POST)
 		if form.is_valid():
-			# Si el formulari és vàlid, cream el objectiu
-			
+			# Si el formulari és vàlid, cream el projecte
+			nom = form.cleaned_data['projecte']
+			descripcio = form.cleaned_data['descripcio']
+			presupost = form.cleaned_data['presupost']
+			estat = form.cleaned_data['estat']
+			tipus = form.cleaned_data['tipus']
+			data_inici = str(form.cleaned_data['data_inici'])
+			data_fi = str(form.cleaned_data['data_fi'])
+			vMin = form.cleaned_data['vMin']
+			vMax = form.cleaned_data['vMax']
+			p = Projecte(nom=nom, descripcio=descripcio, presupost=presupost, 
+				estat=estat, tipus=tipus, data_inici=data_inici, data_fi=data_fi, 
+				minim=vMin, maxim=vMax)
+			p.save()
 			return render(request, 'index.html')
-	# Request de view de formulari
 	else:
-		form = PrincipiForm()
+		form = ProjecteForm()
 	return render(request, 'Projectes/crear.html', {'form': form})
 
+
 # Mètriques
+
+# Retorna la view de la finestra de mètriques, on es veuen tots el resources creats d'aquest tipus.
 def metriques(request):
     return render(request, 'Metriques/metriques.html')
 
+# Si el Request es GET, retorna la view de creació del resource amb el formulari buit.
+# Si el Request es POST, crea el request amb les dades obtingudes al formulari
 def crearMetrica(request):
-	# Dades enviades desde el form
 	if request.method == 'POST':
 		form = MetricaForm(request.POST)
 		if form.is_valid():
-			# Si el formulari és vàlid, cream el objectiu
+			# Si el formulari és vàlid, cream la métrica
 			nom = form.cleaned_data['metrica']
 			descripcio = form.cleaned_data['descripcio']
 			unitat = form.cleaned_data['unitat']
@@ -85,7 +109,6 @@ def crearMetrica(request):
 			m = Metrica(nom=nom, descripcio=descripcio, maxim=maxim, minim=minim, objectiu_id=objectiu_id)
 			m.save()
 			return render(request, 'index.html')
-	# Request de view de formulari
 	else:
 		form = MetricaForm() 
 	return render(request, 'Metriques/crear.html', {'form': form})
